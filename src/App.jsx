c2ceed39897ee2b9cdc2aafd0eaba522999694b4
Dildora Muslimova <faithpacifica@yahoo.com>
2025-07-25 12:46:03 +0900

    setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
 	return (
 		<main>
 			<div>
-				<h1>Welcome to GSAP and GTA Animation with React!</h1>
+				<h1 className='text-emerald-600 text-5xl'>Welcome to GSAP and GTA Animation with React!</h1>
 			</div>
 		</main>
 	);
