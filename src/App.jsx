e2c8c2f6745dd7175843656f32d6d053ddbe3453
Dildora Muslimova <faithpacifica@yahoo.com>
2025-07-25 12:37:38 +0900

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+const App = () => {
+	return (
+		<main>
+			<div>
+				<h1>Welcome to GSAP and GTA Animation with React!</h1>
+			</div>
+		</main>
+	);
+};
+
+export default App;
